7504c3b5ca89d0dce11da42a22ce005e8851e0fc
Victor Gu <victor.guadarrama3@gmail.com>
2017-06-23 13:57:00 -0500

    NextJS basic app with about page link

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
+import Link from 'next/link'
+
 const Index = () => (
     <div>
+        <Link href="/about">
+            <button>About page</button>
+        </Link>
         <p>Hello Next.js</p>
     </div>
 )
